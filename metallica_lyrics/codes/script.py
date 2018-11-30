9b2083e774c667cea57b7b5e00497ced19f4e13a
Victor Accete <victor.accete@gmail.com>
2018-11-30 18:48:05 -0300

    Counting number of unique words

--- metallica_lyrics/codes/script.py
+++ metallica_lyrics/codes/script.py
@@ -1,11 +1,7 @@
-# -*- coding: utf-8 -*-
 import pandas as pd
-import unicodecsv
+import unicodecsv 
 
-#created this just for readability
-#def subset_df(column: str, condition: str, df):
-#    return df.loc[df[column] == condition]
-
+######## Part 1 - Creating of metallica subset ########
 with open('../datasets/songdata.csv', 'rb') as f:
     reader = unicodecsv.DictReader(f)
     lyrics_list = list(reader)
@@ -14,5 +10,13 @@ lyrics_df = pd.DataFrame(lyrics_list)
 
 # the following function returns a dataframe with the condition that the artist is metallica
 metallica_lyrics = lyrics_df.loc[lyrics_df['artist'] == 'Metallica']
-#metallica_lyrics = subset_df('artist', 'Metallica', lyrics_df)
-metallica_lyrics = metallica_lyrics.drop(columns='link')
+metallica_lyrics = metallica_lyrics.drop(columns='link') #this column won't be necessary
+
+
+######## Part 2 - Creating words set ########
+words = set() 
+# https://stackoverflow.com/questions/18936957/count-distinct-words-from-a-pandas-data-frame
+metallica_lyrics['text'].str.lower().str.split().apply(words.update) 
+print("Number of unique words: ", len(words))
+
+#TODO: colocar as palavras num dataframe contando as repetições 
