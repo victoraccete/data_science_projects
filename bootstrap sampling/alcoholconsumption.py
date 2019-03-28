d54d460daad20a4ee3b2f7f495f10c5993b83e44
Victor Accete <victor.accete@gmail.com>
2019-03-27 21:13:10 -0300

    Applying bootstrap technique

--- bootstrap sampling/alcoholconsumption.py
+++ bootstrap sampling/alcoholconsumption.py
@@ -3,7 +3,18 @@ import pandas as pd
 
 mat = pd.read_csv('data_science_projects/bootstrap sampling/student-mat.csv')
 #por = pd.read_csv('data_science_projects/bootstrap sampling/student-por.csv')
-
 mat = mat.drop(columns=['school', 'address', 'reason', 'nursery', 'famsize', 
                         'Pstatus', 'Mjob', 'Fjob', 'traveltime', 'schoolsup',
-                        'famsup'])
+                        'famsup', 'guardian', 'paid', 'activities', 'higher',
+                        'internet', 'romantic', 'famrel', 'freetime', 'goout',
+                        'health'])
+
+bs_sample = pd.DataFrame()
+N = 1000 # tamanho da minha amostra bs
+for i in range(0, N):
+    bs_sample = bs_sample.append(mat.sample(replace=True))
+
+#mat.describe()
+#bs_sample.describe()
+mat.mean()
+bs_sample.mean()
